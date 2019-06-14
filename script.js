0af7c30cef4389c264e44685bf76783aeaef0c8f
Esau Kallan <esau@Esaus-MacBook-Pro.local>
2019-06-14 10:59:02 +0200

    javascript done

--- script.js
+++ script.js
@@ -2,11 +2,30 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
-    document.querySelector("#mere").addEventListener("click", readMore);
+    document.querySelector("#mere1").addEventListener("click", function () {
+        readMore("#tema1");
+    });
+    document.querySelector("#mere2").addEventListener("click", function () {
+        readMore("#tema2");
+    });
+    document.querySelector("#mere3").addEventListener("click", function () {
+        readMore("#tema3");
+    });
+    document.querySelector("#mere4").addEventListener("click", function () {
+        readMore("#tema4");
+    });
+    document.querySelector("#mere5").addEventListener("click", function () {
+        readMore("#tema5");
+    });
+    document.querySelector("#mere6").addEventListener("click", function () {
+        readMore("#tema6");
+    });
+
 }
 
-function readMore() {
+function readMore(id) {
     console.log('readMore:');
-    document.querySelector("#tema1").classList.toggle("stor");
-    document.querySelector("#tema1").classList.toggle("lille");
+    document.querySelector(id).classList.toggle("stor");
+    document.querySelector(id).classList.toggle("lille");
+
 }
